485327cbec501993fc4277fc13d546c11d6453b8
Abubakr <thefox5000@gmail.com>
2024-06-23 17:43:55 +0300

    Create job-card, section-heading and tag components

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,15 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Tamakan</div>
+    <div class="py-8 font-bold">
+        <p>Fullstack Web Developer</p>
+        <p>Full Time - From $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+        <img class="rounded-xl" src="http://placehold.it/42" alt="">
+    </div>
+</div>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,21 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job Finder</title>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 
-<body>
-    <div>
-        <nav>
+<body class="bg-dark-bg text-white">
+    <div class="px-10">
+        <nav class="flex justify-between py-4 border-b border-red/10">
             <div>
-                <a href="'"><img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="Logo">
+                <a href="'"><img height="92" width="92" src="{{ Vite::asset('resources/images/logo.svg') }}"
+                        alt="Logo">
                     logo
                 </a>
             </div>
-            <div>links</div>
+            <div class="space-x-6 font-bold">
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
             <div>post a job</div>
         </nav>
-        <main>{{ $slot }}</main>
+        <main class="mt-10 max-w-[986px] mx-auto">{{ $slot }}</main>
     </div>
 </body>
 
