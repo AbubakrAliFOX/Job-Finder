9558410fae503a32cb30dec4ee91ac2c03e3166d
Abubakr <thefox5000@gmail.com>
2024-06-24 08:11:14 +0300

    Seed Database with featred / unfeatured jobs

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -4,10 +4,10 @@
         <x-employer-logo />
     </div>
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">Tamakan</a>
-        <h3 class="group-hover:text-blue-600 transition-colors duration-300 font-bold text-xl mt-3">Fullstack Web
+        <a href="#" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
+        <h3 class="group-hover:text-blue-600 transition-colors duration-300 font-bold text-xl mt-3">{{ $job->title }}
             Developer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->schedule }} - From {{ $job->salary }}</p>
     </div>
     <div class="">
         @foreach ($job->tags as $tag)
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,10 +1,10 @@
 @props(['job'])
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Tamakan</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
     <div class="py-8">
-        <h3 class="group-hover:text-blue-600 transition-colors duration-300 font-bold text-xl">Fullstack Web Developer
+        <h3 class="group-hover:text-blue-600 transition-colors duration-300 font-bold text-xl">{{ $job->title }}
         </h3>
-        <p class="text-sm mt-4">Full Time - From $60,000</p>
+        <p class="text-sm mt-4">{{ $job->schedule }} - From {{ $job->salary }}</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
